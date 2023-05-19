f4dc42cf4d7f5c2cc908147d7f88c32528117eea
VeronikaKan <kan.v98@gmail.com>
2023-05-19 21:49:12 +0600

    add section partners and footer

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        margin:30,
+        items:5,
+        autoplay:true,
+        autoplayTimeout:1000,
+        loop:true
+    });
+  });
